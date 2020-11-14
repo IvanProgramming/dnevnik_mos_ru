8846dda90727fa39f3fc802e9530ce42bd92fb0e
IvanProgramming <vlasov.ivan.2004@gmail.com>
2020-11-15 02:24:58 +0300

    Some optimizations, for making main.py work correctly.

--- dnevnik/__init__.py
+++ dnevnik/__init__.py
@@ -1,6 +1,6 @@
+from .school import School
 from .class_unit import ClassUnit
 from .group import Group
-from .school import School
 from .teacher import Teacher
 from .student_profile import StudentProfile
 from .client import Client
