a9a416bb6fd667f173c20997251fcd7dba60e7a8
IvanProgramming <vlasov.ivan.2004@gmail.com>
2020-11-17 13:30:45 +0300

    Still prepairng for publish

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup, find_packages
 
-from dnevnik import __version__
-
 setup(
     name='dnevnik-mos-ru',
-    version=__version__,
+    version="0.0.3 Alpha",
     url='https://github.com/IvanProgramming/dnevnik_mos_ru',
     author='Ivan Vlasov',
     py_modules=["dnevnik"],
