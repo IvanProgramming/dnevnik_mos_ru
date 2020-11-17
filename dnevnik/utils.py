9443ade0952cf166f5378d6d43159e3218aefff2
IvanProgramming <vlasov.ivan.2004@gmail.com>
2020-11-17 11:03:05 +0300

    Added function to sort lessons

--- dnevnik/utils.py
+++ dnevnik/utils.py
@@ -15,3 +15,15 @@ def reformat_date(date_str: str) -> datetime:
 def reformat_birthday_date(date_str: str) -> datetime:
     """ Превращает дату рождения в объект datetime """
     return datetime.strptime(date_str, "%d.%m.%Y")
+
+
+def remove_unused_keys(unused_keys_list: list, source: dict):
+    """ Позволяет по списку ключей удалить из словаря ненужные значения """
+    for unused_key in unused_keys_list:
+        if unused_key in source:
+            del source[unused_key]
+    return source
+
+
+def sort_lessons(lessons):
+    return sorted(lessons, key=lambda l: l.lesson_number)
