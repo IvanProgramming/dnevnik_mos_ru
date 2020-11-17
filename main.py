f2fc89ebe1a23062bcb3e33c39a799348282bd9a
IvanProgramming <vlasov.ivan.2004@gmail.com>
2020-11-17 10:33:32 +0300

    Changed testing script

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from datetime import datetime
 from pprint import pp
 
 from dnevnik import Client
 from testing_directory.settings import *
+
 me = Client(AUTH_TOKEN, PROFILE_ID)
-pp(me.get_homeworks(datetime(2020, 11, 16), datetime(2020, 11, 16)))
+pp(me.make_request("/jersey/api/schedule_items", group_id=",".join(map(lambda gr: str(gr.id), me.profile.groups)),
+                   **{"from": "2020-11-16"}, to="2020-11-16", with_group_class_subject_info=True))
