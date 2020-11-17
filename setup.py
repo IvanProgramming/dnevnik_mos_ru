3be0d9d579cd14800ea944f657f05fbf25c1f1e1
IvanProgramming <vlasov.ivan.2004@gmail.com>
2020-11-17 20:13:43 +0300

    Added description and new version

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
+from dnevnik import __version__
 
 setup(
     name='dnevnik-mos-ru',
-    version="0.0.5 Alpha",
+    version=__version__,
+    description="This package is kind of wrapper for dnevnik.mos.ru API service",
     url='https://github.com/IvanProgramming/dnevnik_mos_ru',
     author='Ivan Vlasov',
     packages=find_packages(),
