fbc0146dd22094ac75e64e48c07e36e9f914840c
IvanProgramming <vlasov.ivan.2004@gmail.com>
2020-11-17 14:50:19 +0300

    IT WORKS!!!!!!!!

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='dnevnik-mos-ru',
-    version="0.0.3 Alpha",
+    version="0.0.5 Alpha",
     url='https://github.com/IvanProgramming/dnevnik_mos_ru',
     author='Ivan Vlasov',
-    py_modules=["dnevnik"],
+    packages=find_packages(),
     install_requires=[
         'requests',
     ]
