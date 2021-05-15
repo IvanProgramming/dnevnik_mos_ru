792946de4dbc180324d6825cd896196910e5709e
IvanProgramming <vlasov.ivan.2004@gmail.com>
2021-05-15 20:24:12 +0300

    Added creditionals error for Selenium Authorization

--- dnevnik/auth_providers/selenium_auth.py
+++ dnevnik/auth_providers/selenium_auth.py
@@ -1,6 +1,8 @@
 from time import sleep, time
 from ..base_auth_provider import BaseAuthProvider
+from ..exceptions import CredentialsInvalidException
 from selenium import webdriver
+from urllib3.util import parse_url
 
 
 class SeleniumAuthorization(BaseAuthProvider):
@@ -30,13 +32,18 @@ class SeleniumAuthorization(BaseAuthProvider):
         submit_button.click()
 
         auth_token = driver.get_cookie("auth_token")
+        while parse_url(driver.current_url).host != "dnevnik.mos.ru":
+            if driver.find_element_by_css_selector('blockquote.blockquote-danger'):
+                raise CredentialsInvalidException(driver.find_element_by_css_selector('blockquote.blockquote-danger'))
+
         while not auth_token:
             auth_token = driver.get_cookie("auth_token")
-            sleep(0.2)
+            sleep(0.01)
+
         self.profile_id = int(driver.get_cookie("profile_id")["value"])
         driver.close()
         execution_time = time() - start_time
-        self.auth_token = auth_token
+        self.auth_token = auth_token['value']
 
     def refresh_token(self):
         self.proceed_authorization()
