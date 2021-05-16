cb8cf4fb5ef63971c5d055d55e79ed12ec16afe5
IvanProgramming <vlasov.ivan.2004@gmail.com>
2021-05-16 10:53:59 +0300

    Added properties docs

--- dnevnik/academic_years.py
+++ dnevnik/academic_years.py
@@ -5,8 +5,11 @@ from pydantic import BaseModel
 class AcademicYear(BaseModel):
     """ Объект учебного года """
     id: int
+    # Название года для показа ({год-начала} - {год-конца})
     name: str
+    # Даты начала и конца учебного года
     begin_date: date
     end_date: date
     calendar_id: int
+    # Текущий ли год?
     current_year: bool
