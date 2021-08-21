5607480e374fdb0a6aad171aac841142e012e122
Ivan <vlasov.ivan.2004@gmail.com>
2021-08-21 12:51:32 +0300

    :two_men_holding_hands: Fake Provider Token format and profile update

--- model/diary_providers/fake_diary_provider.py
+++ model/diary_providers/fake_diary_provider.py
@@ -1,23 +1,33 @@
+from base64 import b64decode
+from json import loads
+
 from exceptions.providers import TokenInvalidException
 from model.diary_providers.base_diary_provider import BaseDiaryProvider
+from model.profile import Profile
 
 
 class FakeDiaryProvider(BaseDiaryProvider):
     """
     This is parabola's fake diary provider
-    Token format: {any15symbols}:{phoneNumber}(70000000000)
+    Token format: {any15symbols}: (json with fields "name", "")
     """
     name = "Parabola Fake Diary"
     unique_name = "fake"
 
     @staticmethod
     async def is_token_correct(token: str) -> bool:
-        return len(token) == 27 and len(token.split(":")[0]) == 15
+        return True
 
     @staticmethod
     async def get_phone_number(token: str) -> str:
-        print(len(token))
         if await FakeDiaryProvider.is_token_correct(token):
-            return token.split(":")[1]
+            token_json = loads(b64decode(token.split(":")[1]).decode("utf-8"))
+            return token_json["phone_number"]
         raise TokenInvalidException
 
+    @staticmethod
+    async def get_profile_instance(token: str) -> Profile:
+        if await FakeDiaryProvider.is_token_correct(token):
+            token_json = loads(b64decode(token.split(":")[1]).decode("utf-8"))
+            return Profile(**token_json)
+        raise TokenInvalidException
