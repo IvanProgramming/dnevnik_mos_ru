3764a1099fb12e05c90a216f7f7d1223a2eff7cd
Ivan <vlasov.ivan.2004@gmail.com>
2021-08-21 12:00:21 +0300

    :computer: Added profiles_db instance (represents connection to profiles_collection)

--- model/connections.py
+++ model/connections.py
@@ -8,11 +8,13 @@ class Connections:
     """ Connection is special object, that is used to store ALL connections of this back-end """
     mongo_client: MongoClient
     tokens_db: Collection
+    profiles_db: Collection
 
     def start_connections(self):
         """ This method is used for starting all connections """
         self.mongo_client = MongoClient(DB_URL)
         self.tokens_db = self.mongo_client.db.tokens_collection
+        self.profiles_db = self.mongo_client.db.profiles_collection
         self.setup_indexes()
 
     def setup_indexes(self):
