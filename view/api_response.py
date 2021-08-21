47a240fe28dcc88aed996662ae308f653b33d1fb
Ivan <vlasov.ivan.2004@gmail.com>
2021-08-21 12:02:36 +0300

    :earth_americas: Api response model changed

--- view/api_response.py
+++ view/api_response.py
@@ -6,11 +6,13 @@ from starlette.responses import JSONResponse
 
 class OKResponse(JSONResponse):
     """ This type of response is JSONResponse, but with OK status and data wrap """
+
     def render(self, content: Any):
-        return dumps({"status": "ok", "data": content}, ensure_ascii=False).encode("utf-8")
+        return dumps({"status": True, "data": content}, ensure_ascii=False).encode("utf-8")
 
 
 class ErrorResponse(JSONResponse):
     """ The same as previous, but with error status """
+
     def render(self, content: Any):
-        return dumps({"status": "error", "data": content}, ensure_ascii=False).encode("utf-8")
+        return dumps({"status": False, "data": content}, ensure_ascii=False).encode("utf-8")
