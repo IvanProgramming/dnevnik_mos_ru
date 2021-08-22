761a83cd70b8f0a62a826fe62e8ac03f6deee8cf
Ivan <vlasov.ivan.2004@gmail.com>
2021-08-22 12:25:04 +0300

    :snake: Removed unused import

--- controller/api_methods/dev_methods.py
+++ controller/api_methods/dev_methods.py
@@ -1,7 +1,6 @@
 """
     API methods for devs, works only in DEBUG mode
 """
-from starlette.responses import JSONResponse
 from starlette.schemas import SchemaGenerator
 
 
@@ -10,8 +9,6 @@ def get_openapi_schema(request):
             200:
                 description: OpenAPI schema
     """
-    # I'm using this for avoiding cycle import error
-    from routes import APP_ROUTES
     schemas = SchemaGenerator(
         {"openapi": "3.0.0", "info": {"title": "Lastic REST API", "version": "1.0.0"}}
     )
