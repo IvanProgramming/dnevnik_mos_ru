ea8d2d23e6415d1860be42f727ba7fde396ac87c
Ivan <vlasov.ivan.2004@gmail.com>
2021-08-23 01:04:36 +0300

    :two_men_holding_hands: Event sharing added

--- controller/api_methods/__init__.py
+++ controller/api_methods/__init__.py
@@ -1,4 +1,5 @@
 from .dev_methods import get_openapi_schema
+from .events import EventEndpoint
 from .friends import FriendsEndpoint, search_friend
 from .profile import ProfileEndpoint
 from .service import ping
