f02c5a680f88f470d70d979c27e0042d77b9307f
Ivan <vlasov.ivan.2004@gmail.com>
2021-08-22 12:02:51 +0300

    :recycle: Profile edit and Friends search added. Also added nickname

--- model/profile.py
+++ model/profile.py
@@ -1,11 +1,16 @@
-from exceptions.profiles import SelfPhoneNumberError, InvalidPhoneNumberError, AlreadyInFriendsError
+from typing import List
+
+from controller.utils import normalize_phones, normalize_phone
+from exceptions.profiles import SelfPhoneNumberError, InvalidPhoneNumberError, AlreadyInFriendsError, \
+    EmojiIsNotSupported, ColorModelError, NicknameTooLongError
 from model.connections import connections
 from model.friend_profile import FriendProfile
 from settings import AVAILABLE_EMOJI
 
 
 class Profile:
-    def __init__(self, name: str, phone_number: str, school_name: str, friends=None, _id=0, color=None, emoji=None):
+    def __init__(self, name: str, phone_number: str, school_name: str, friends=None, _id=0, color=None, emoji=None,
+                 nickname=None):
         """ Constructor of model """
         if friends is None:
             friends = []
@@ -13,8 +18,11 @@ class Profile:
             color = Profile.generate_color(phone_number, name)
         if emoji is None:
             emoji = Profile.generate_emoji(phone_number, name)
+        if nickname is None:
+            nickname = name
         self.name = name
-        self.phone_number = phone_number
+        self.nickname = nickname
+        self.phone_number = normalize_phone(phone_number)
         self.school_name = school_name
         self.color = color
         self.emoji = emoji
@@ -27,13 +35,14 @@ class Profile:
         if not Profile.exists(self.phone_number):
             new_profile = {
                 "name": self.name,
-                "phone_number": self.phone_number,
+                "nickname": self.name,
+                "phone_number": normalize_phone(self.phone_number),
                 "school_name": self.school_name,
                 "friends": self.friends,
                 "color": self.color,
                 "emoji": self.emoji
             }
-            new_id = connections.profiles_db.insert_one(new_profile)
+            new_id = connections.profiles_db.insert_one(new_profile).inserted_id
             self._id = new_id
         else:
             self.sync_with_db()
@@ -47,6 +56,7 @@ class Profile:
             self.emoji = db_object["emoji"]
             self.name = db_object["name"]
             self._id = db_object["_id"]
+            self.nickname = db_object["nickname"]
         else:
             self.register_new()
 
@@ -67,6 +77,7 @@ class Profile:
         """ Makes json from profile for sending it as a response """
         return {
             "name": self.name,
+            "nickname": self.nickname,
             "phone_number": self.phone_number,
             "school_name": self.school_name,
             "friends_count": self.count_friends(),
@@ -85,7 +96,7 @@ class Profile:
 
     def add_friend(self, phone_number: str):
         """ Adding friend by his phone_number """
-        # YES IT LOOKS SCARY, BUT I THINK IT IS THE BEST SOLUTION HERE
+        phone_number = normalize_phone(phone_number)
         if not self.exists(phone_number):
             raise InvalidPhoneNumberError
         if phone_number in self.friends:
@@ -94,6 +105,7 @@ class Profile:
             raise SelfPhoneNumberError
         connections.profiles_db.update_one({"phone_number": self.phone_number},
                                            {"$push": {"friends": phone_number}})
+        self.friends.append(phone_number)
         is_online = bool(connections.tokens_db.count_documents({"phone_number": phone_number}))
         return FriendProfile(**Profile.profile_by_phone(phone_number).as_json(), is_online=is_online).dict()
 
@@ -117,14 +129,40 @@ class Profile:
         })
         return Profile.parse_friends_cursor(request_cursor, as_dict)
 
+    def edit_profile(self, nickname=None, emoji=None, color=None):
+        """ Edits user profile """
+        if nickname is None:
+            nickname = self.nickname
+        if emoji is None:
+            emoji = self.emoji
+        if color is None:
+            color = self.color
+
+        if type(color) != list \
+                or len(color) != 3 \
+                or max(color) > 255 \
+                or min(color) < 0:
+            raise ColorModelError
+        if emoji not in AVAILABLE_EMOJI:
+            raise EmojiIsNotSupported
+        if not 0 < len(nickname) <= 16:
+            raise NicknameTooLongError
+        connections.profiles_db.update_one({"phone_number": self.phone_number}, {"$set": {
+            "nickname": nickname,
+            "emoji": emoji,
+            "color": color
+        }})
+
     @staticmethod
     def parse_friends_cursor(friends_cursor, as_dict=True):
-        """ Parses MongoDB query to FriendProfile list """
+        """ Parses MongoDB query to FriendProfile list]
+            :param as_dict: Shows, if returned result should be a dict
+        """
         friends_list = []
         phones_list = []
         for friend in friends_cursor:
             friend_profile = Profile(**friend)
-            friends_list.append(FriendProfile(**friend_profile.as_json(), is_online=False))
+            friends_list.append(friend_profile.as_friend_profile(fetch_online=False))
             phones_list.append(friend_profile.phone_number)
         online_phones = list(map(lambda x: x["phone_number"], connections.tokens_db.find({
             "phone_number": {"$in": phones_list}
@@ -142,7 +180,7 @@ class Profile:
     @staticmethod
     def exists(phone_number: str):
         """ Checks is profile exists """
-        return bool(connections.profiles_db.find_one({"phone_number": phone_number}))
+        return bool(connections.profiles_db.find_one({"phone_number": normalize_phone(phone_number)}))
 
     @staticmethod
     def generate_color(phone_number, name):
@@ -167,12 +205,23 @@ class Profile:
     @staticmethod
     def profile_by_phone(phone_number):
         """ Returns profile by phone number """
+        phone_number = normalize_phone(phone_number)
         result = connections.profiles_db.find_one({"phone_number": phone_number})
         if result:
-            return Profile(phone_number=result['phone_number'],
-                           name=result["name"],
-                           school_name=result["school_name"])
+            return Profile(**result)
         return None
+
+    @staticmethod
+    def fetch_existing(phone_numbers: List[str]):
+        """ Returns friends profiles from contact book (passed in phone_numbers)"""
+        phone_numbers = normalize_phones(phone_numbers)
+        exists_friends = connections.profiles_db.find({
+            "phone_number": {"$in": phone_numbers}
+        })
+        friends = []
+        for friend in exists_friends:
+            friends.append(Profile(**friend).as_friend_profile().dict())
+        return friends
 
     def as_friend_profile(self, fetch_online=False):
         if fetch_online:
