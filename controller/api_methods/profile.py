1f6645caf3d3f2d1fc5922101ff0185fb2343d34
Ivan <vlasov.ivan.2004@gmail.com>
2021-08-22 12:03:42 +0300

    :earth_americas: Added profile edition method

--- controller/api_methods/profile.py
+++ controller/api_methods/profile.py
@@ -5,7 +5,7 @@ from view.api_response import OKResponse
 
 
 class ProfileEndpoint(HTTPEndpoint):
-    def get(self, request: Request):
+    async def get(self, request: Request):
         """
         responses:
             200:
@@ -13,4 +13,24 @@ class ProfileEndpoint(HTTPEndpoint):
                 example: {"status":true,"data":{"name":"Ivan","phone_number":"79999999999","school_name":"Some's school","friends_count":1,"color":[201,146,208],"emoji":"cookie"}}
         """
         profile = request.state.profile
+        return OKResponse(profile.as_json())
+
+    async def patch(self, request: Request):
+        """
+        responses:
+            200:
+                description: Succesfuly edited
+                example: {"status":true,"data":{"name":"Ivan","phone_number":"79999999999","school_name":"Some's school","friends_count":1,"color":[201,146,208],"emoji":"cookie"}}
+            403:
+                description: Emoji is not allowed
+            400:
+                description: Error in request data
+        """
+        profile = request.state.profile
+        data = await request.json()
+        profile.edit_profile(
+            nickname=data["nickname"],
+            emoji=data["emoji"],
+            color=data["color"]
+        )
         return OKResponse(profile.as_json())
